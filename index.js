d52dd5e3ef9d3669fbb1c0134580382eda6427cd
Tiffany_Tam <tamtiffany89@gmail.com>
2021-10-19 12:42:12 -0400

    commit tree 197a6ee954be8b80cf097afb57eb8aa9666e3f13

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// const button = document.getElementById("notify") button.addEventListener("DOMContentLoaded",functions(){
+//     console.log("This is really cool!");
+//   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+      let p = document.querySelector('p')
+      p.textContent = 'This is really cool!'
+      console.log(p);
+      document.querySelector('#text').appendchild(p);
+  })
